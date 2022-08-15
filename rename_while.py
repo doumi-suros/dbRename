84215d96f25ce95a87f50adc0ba7fd124a49e175
ESTHER <110657119+suros-esther@users.noreply.github.com>
2022-08-15 16:42:34 +0800

    Add files via upload

--- rename_while.py
+++ rename_while.py
@@ -2,29 +2,40 @@ import os
 import pandas
 
 
-reptPath = 'C:\\SurosBI\\Panorays_Reports_220810A\\'
-excelFile = 'dbData_rename_220812.xlsx'
+reptPath = 'C:\\SurosBI\\Panorays_Reports_220815A\\'
+excelFile = 'dbData_rename_220815.xlsx'
 reptList = pandas.read_excel(excelFile, sheet_name=0, usecols=[1,2,3,4,5,6,7])
 reptHead = ['報告檔案夾', 'Date', 'pdfName', 'csvName', 'NewDate','NewpdfName','NewcsvName']
 
 
-for m in range (15, 22):
+for m in range (1, 1673):
+	
+	print(str(m) + ".start")
 	reptFolder = reptList[reptHead[0]].values[m].strip()    #get report dir
 	iniPdf = reptList[reptHead[2]].values[m].strip()    #get initial pdf file name
 	iniCsv = reptList[reptHead[3]].values[m].strip()    #get initial csv file name
 	revisPdf = reptList[reptHead[5]].values[m].strip()	 #get revised pdf file name
 	revisCsv = reptList[reptHead[6]].values[m].strip()	 #get revised csv file name
+	
+	if (iniPdf == 'no.pdf') and (iniCsv =='no.csv'):
+		print(str(m) + ".no folder, pass")
+		pass
 
 	while iniPdf != 'no.pdf':
 		inipdfPath = reptPath + reptFolder + '\\' + iniPdf    #get initial pdf path
 		revispdfPath = reptPath + reptFolder + '\\' + revisPdf  #get revised pdf path
 		os.rename(inipdfPath,revispdfPath)
+		print(str(m) + ".loopPDF")
+		break
 
 	while iniCsv != 'no.csv':
 		inicsvPath = reptPath + reptFolder + '\\' + iniCsv    #get initial csv path
 		reviscsvPath = reptPath + reptFolder + '\\' + revisCsv  #get revised csv path
 		os.rename(inicsvPath,reviscsvPath)
+		print(str(m) + ".loopCSV")	
+		break	
 
+	print(str(m) + ".done")
 	m+=1
 
 
